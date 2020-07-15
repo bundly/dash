6164017b7c81a38bae7132c7ac757f8cbbd9052f
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2020-07-15 18:14:19 +0530

    user action

---
+++ src/actions/myaction.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+export const fetchUserAction = ()=>{
+   return (dispatch)=>{
+    axios.get('/api/current_user')
+    .then((res)=>{
+       dispatch({type:'GET_USER',payload:res.data})
+    })
+   }
+}
