728331dfd909c3e5e05169982098eb46dc4e264e
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-07-15 17:27:46 +0530

    `server`: added yarn, fixed app.js

--- server/app.js
+++ server/app.js
@@ -1,54 +1,55 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const flash = require('connect-flash');
-const session = require('express-session');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
 
 // Passport config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Connect to Mongo
-mongoose.connect('mongodb://localhost:27017/users', { useNewUrlParser: true })
-    .then(console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
+mongoose
+  .connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB service"))
+  .catch((err) => console.log(err));
 
-//EJS
-app.use(expressLayouts);
-
-// Bodyparser 
-app.use(express.urlencoded({extended: false}));
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
 
 // Express Session
 app.use(
-    session({
-        secret: 'secret',
-        resave: true,
-        saveUninitialized: true
-    })
+  session({
+    secret: process.env.APP_SECRET || "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
 );
 
-app.use(passport.initialize()); 
-app.use(passport.session());    
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Connect Flash
 app.use(flash());
 
 // Global variables
-app.use(function(req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
 });
 
 //Routes
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
-})
+  console.log(`Server started on port ${PORT}`);
+});
