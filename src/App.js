a7200f8014fd4d9cc1c6629e85580fa2106253d4
Darsh <darshkpatel@gmail.com>
2020-07-15 18:22:13 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import "./App.css";
 import Header from "./components/Header";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./components/Home";
