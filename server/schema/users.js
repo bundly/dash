f8e2f160b901906b187b8acda8d06159c359f895
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-07-15 21:25:48 +0530

    feat: schema updated

--- server/schema/users.js
+++ server/schema/users.js
@@ -1,20 +1,26 @@
 import mongoose from 'mongoose';
 
 const usersSchema = new mongoose.Schema({
+    // Github username
     username: {
         type: String
     },
-    protected: {
-        type: Boolean,
-        default: false
-    },
-    password: {
-        type: String
-    },
-    teams: {
-        type: Array,
-        default: []
-    }
+    accounts: [
+        // { kind: 'github', uid: 'fred.rogers' }
+        {
+            kind: String,
+            uid: String,
+            // {
+            //     "access_token": "6qrZcUqja7812RVdnEKjpzOL4CvHBFG",
+            //     "token_type": "Bearer",
+            //     "expires_in": 604800,
+            //     "refresh_token": "D43f5y0ahjqew82jZ4NViEr2YafMKhue",
+            //     "scope": "identify"
+            // }
+            token: Object
+        }
+    ],
+    commits: Number
 });
 
 export default usersSchema;
