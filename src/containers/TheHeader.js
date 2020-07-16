10d2a7aadaa0db620edae7e810d01a7b03cc99fe
Darsh <darshkpatel@gmail.com>
2020-07-16 21:38:18 +0530

    Cleanup Header and dashboard

--- src/containers/TheHeader.js
+++ src/containers/TheHeader.js
@@ -16,7 +16,7 @@ import CIcon from '@coreui/icons-react'
 // routes config
 import routes from '../routes'
 
-import { 
+import {
   TheHeaderDropdown,
   TheHeaderDropdownMentions,
   TheHeaderDropdownTasks
@@ -52,45 +52,15 @@ const TheHeader = () => {
         <CIcon name="logo" height="48" alt="Logo"/>
       </CHeaderBrand>
 
-      <CHeaderNav className="d-md-down-none mr-auto">
-        <CHeaderNavItem className="px-3" >
-          <CHeaderNavLink to="/dashboard">Dashboard</CHeaderNavLink>
-        </CHeaderNavItem>
-        <CHeaderNavItem  className="px-3">
-          <CHeaderNavLink to="/users">All Users</CHeaderNavLink>
-        </CHeaderNavItem>
-        <CHeaderNavItem className="px-3">
-          <CHeaderNavLink>Settings</CHeaderNavLink>
-        </CHeaderNavItem>
-      </CHeaderNav>
 
-      <CHeaderNav className="px-3">
-        <TheHeaderDropdownTasks/>
-        <TheHeaderDropdownMentions/>
-        <TheHeaderDropdown/>
-      </CHeaderNav>
 
-      <CSubheader className="px-3 justify-content-between">
-        <CBreadcrumbRouter 
-          className="border-0 c-subheader-nav m-0 px-0 px-md-3" 
-          routes={routes} 
+      <CHeaderNav>
+        <CBreadcrumbRouter
+          className="border-0 c-subheader-nav m-0 px-0 px-md-3"
+          routes={routes}
         />
-          <div className="d-md-down-none mfe-2 c-subheader-nav">
-            <CLink className="c-subheader-nav-link"href="#">
-              <CIcon name="cil-speech" alt="Settings" />
-            </CLink>
-            <CLink 
-              className="c-subheader-nav-link" 
-              aria-current="page" 
-              to="/dashboard"
-            >
-              <CIcon name="cil-graph" alt="Dashboard" />&nbsp;Dashboard
-            </CLink>
-            <CLink className="c-subheader-nav-link" href="#">
-              <CIcon name="cil-settings" alt="Settings" />&nbsp;Settings
-            </CLink>
-          </div>
-      </CSubheader>
+      </CHeaderNav>
+
     </CHeader>
   )
 }
--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,26 +1,11 @@
 import React from "react";
 import { CCard, CCardBody, CCardHeader, CCol, CRow } from "@coreui/react";
-import Iframe from "react-iframe";
-// import CIcon from "@coreui/icons-react";
-
 const Dashboard = () => {
   return (
     <>
       <CRow>
         <CCol>
-          <CCard>
-            <CCardHeader>Fellowship Google Calendar</CCardHeader>
-            <CCardBody>
-              <Iframe
-                src="https://calendar.google.com/calendar/embed?height=800&amp;wkst=2&amp;bgcolor=%2305c4e2&amp;ctz=Asia%2FKolkata&amp;src=bWFqb3JsZWFndWVoYWNraW5nLmNvbV9wcjNuampoNG9rMHBpOTNqZnFtNTFqZzJnMEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23E67C73&amp;showNav=1&amp;mode=AGENDA&amp;title=MLH%20Fellowship"
-                style="border-width:0"
-                width="100%"
-                height="400"
-                frameborder="0"
-                scrolling="no"
-              />
-            </CCardBody>
-          </CCard>
+
         </CCol>
       </CRow>
     </>
