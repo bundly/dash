4a626a7229d3196ff60343e9754e80eacede9980
Darsh <darshkpatel@gmail.com>
2020-07-17 00:41:21 +0530

    seach api complete

--- src/views/workspace/search/Search.js
+++ src/views/workspace/search/Search.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import { CCard, CCardBody, CCardHeader, CCol, CRow } from "@coreui/react";
 import ReactMarkdown from 'react-markdown';
-
+import {githubSearch} from '../../../scripts/githubAPI'
 import SearchBar from "@bit/lekanmedia.shared-ui.search-bar";
 
 const style = {
@@ -13,24 +13,35 @@ const style = {
   fontSize: "1.5em",
 };
 
-const showResults = () => {
-  const results = [];
-  for (let index = 0; index < 4; index++) {
-    results.push(
+const processResults = (searchResults) => {
+  const results = searchResults.map(result=>{
+    return (
       <CCol xs="12" sm="6" md="12">
         <CCard accentColor="success">
-          <CCardHeader><b>Github: </b>@Kunal made a Pull Request for #1</CCardHeader>
+    <CCardHeader><b><a href={result.html_url}>Title: {result.title}</a></b><a href={result.repository_url.replace('api.','').replace('repos/','')}>{'      '}{result.repository_url.replace('https://api.github.com/repos/','')}</a> Search Score: {result.score}</CCardHeader>
           <CCardBody>
-            <ReactMarkdown source={PRinfo} />
+            <ReactMarkdown source={result.body} />
           </CCardBody>
         </CCard>{" "}
       </CCol>
     );
-  }
-
+  })
   return results;
 };
 const Search = () => {
+  const [done,setDone] = useState(false)
+  const [results,setResults] = useState([])
+  const [count,setCount] = useState(undefined)
+  const handleSearch = (query) => {
+    setDone(false)
+    githubSearch(query).then((res)=>{
+      setCount(res.data.total_count)
+      setResults(res.data.items)
+      setDone(true)
+    })
+  }
+
+
   return (
     <>
       <CCard>
@@ -38,20 +49,21 @@ const Search = () => {
           Search your discord and github
           <div className="card-header-actions">
             <a href="https://github.com/bundly" className="card-header-action">
-              <small className="text-muted">@discord</small>
+              {/* <small className="text-muted">@discord</small> */}
             </a>
           </div>
         </CCardHeader>
         <CCardBody>
           <div className="card text-center">
             <div style={style}>
-              <SearchBar placeholder="Search here..." />
+              <SearchBar placeholder="Search here..." done={done} onSearch={(e)=>handleSearch(e)} />
             </div>
           </div>
         </CCardBody>
         <CCardBody>
+          {count>0?`Found ${count} results`:''}
           <CRow>
-            {showResults()}
+            {processResults(results)}
           </CRow>
         </CCardBody>
       </CCard>
