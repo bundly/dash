d3bda0abb3999eb74b365f2dba6c5519c1e1835e
Darsh <darshkpatel@gmail.com>
2020-07-16 15:47:13 +0530

    temp helper functions

--- src/views/notes/helpers.js
+++ src/views/notes/helpers.js
@@ -3,3 +3,20 @@ export const getUsername = () => {
 
  return 'darshkpatel' // Temporary till OAuth done
 }
+
+export const getToDo = () => {
+
+   return {
+    markdown: `
+    ## To Do
+     - Example Task LOADED
+     `,
+    lastUpdated: new Date()
+  } // Temporary till OAuth done
+  }
+
+export const saveToDo = (username, value) => {
+    // Get Username
+    console.log(`saving for ${username}`, value)
+     return; // // Temporary till OAuth done
+}
