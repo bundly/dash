9e8985f444b3d0472f6c91feca98cc21b240d4b4
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-07-23 15:20:12 +0530

    hotfix: discord token validation

--- src/scripts/discordAPI.js
+++ src/scripts/discordAPI.js
@@ -2,7 +2,9 @@ export function getToken() {
   const bundlyToken = localStorage.getItem("bundly-token");
   let discordToken;
   if (bundlyToken) {
-    discordToken = JSON.parse(atob(bundlyToken)).tokens[1].token.accessToken;
+    if (JSON.parse(atob(bundlyToken)).tokens[1]) {
+      discordToken = JSON.parse(atob(bundlyToken)).tokens[1].token.accessToken;
+    }
   }
   return {
     token: discordToken,
