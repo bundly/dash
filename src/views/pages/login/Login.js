b525fc14f1f7774628574b0c148ac0bdb01a37c7
Kartik Choudhary <kartikc.918@gmail.com>
2020-07-27 08:33:40 +0000

    Update login design and layout fixes (#43)
    * removed icon, fix login layout, update design
    
    * Increase graphic size, update layout
    
    * Update login.module.scss
    
    Co-authored-by: Saurav M H <sauravhiremath@gmail.com>
    
    * Remove login-github media queries
    
    Co-authored-by: Saurav M H <sauravhiremath@gmail.com>

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { default as axios } from "axios";
 
-import { host } from '../../../App';
+import { host } from "../../../App";
 import styled from "../../../scss/login.module.scss";
 
 const loginUrl = `${host}/auth/github/login`;
@@ -16,44 +16,18 @@ const Login = ({ authenticate }) => {
   }, [authenticate]);
 
   return (
-    <div className="container fluid">
+    <div className="container-fluid">
       <div className={styled["landing-page-container"]}>
         <div className={styled.content__wrapper}>
           <header className={styled.header}>
-            <h1 className="d-flex justify-content-center display-1">BUNDLY</h1>
-            <ul
-              className={`${styled["social-container"]} ${styled.header__item}`}
-            >
-              <li
-                className={`${styled.social__icon} ${styled["social__icon--fb"]}`}
-              >
-                <img
-                  src="https://image.flaticon.com/icons/svg/2111/2111425.svg"
-                  alt="github"
-                />
-              </li>
-              <li
-                className={`${styled.social__icon} ${styled["social__icon--dr"]}`}
-              >
-                <img
-                  src="https://image.flaticon.com/icons/svg/841/841568.svg"
-                  alt="website"
-                />
-              </li>
-            </ul>
+            <h1 className="d-flex justify-content-center display-3">BUNDLY</h1>
           </header>
           <p className={styled.coords}>S 75° 6' 0.027" / E 123° 19' 59.998"</p>
           <div className={styled["ellipses-container"]}>
-            <div className={styled.greeting}>
-              <img
-                src="https://i.giphy.com/media/l0iVAuEtPF5KRbqmvd/source.gif"
-                alt="MLH thingy"
-              />
-            </div>
+            <div className={styled.greeting}></div>
             <div
               className={`${styled.ellipses} ${styled["ellipses__outer--thin"]}`}
-            >
-            </div>
+            ></div>
             <div
               className={`${styled.ellipses} ${styled["ellipses__outer--thick"]}`}
             />
@@ -68,13 +42,15 @@ const Login = ({ authenticate }) => {
               <span className={styled.timeline__unit} />
             </div>
           </div>
-          <div className={styled["login-github"]}>
-            <a
-              className={`${styled.button} ${styled["button--social-login"]} ${styled["button--github"]}`}
-              href={loginUrl}
-            >
-              <i className={styled["cib-github"]}></i>Login With Github
-            </a>
+          <div className="text-center">
+            <div className={styled["login-github"]}>
+              <a
+                className={`${styled.button} ${styled["button--social-login"]} ${styled["button--github"]}`}
+                href={loginUrl}
+              >
+                Login With Github
+              </a>
+            </div>
           </div>
         </div>
       </div>
