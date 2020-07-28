6e2259af5774f64dedba22433da7400482d243e0
Darsh <darshkpatel@gmail.com>
2020-07-28 11:21:07 +0530

    use docker mongo url

--- server/env.js
+++ server/env.js
@@ -3,9 +3,9 @@ dotenv.config();
 
 export const { DB_NAME, MONGO_PASSWORD, API_KEY, SALT_ROUNDS } = process.env;
 
-export const DB_URL = 'mongodb://localhost:27017/dash';
 export const API_PORT = process.env.API_PORT || 5000;
 
+export let DB_URL = '';
 export let hosts = [];
 export let callbackUrl = '';
 export const TURN_INTERVAL = 0;
@@ -13,8 +13,10 @@ if (process.env.NODE_ENV === 'production') {
     // React App Deployed URLs
     hosts = ['https://bundly.tech', 'https://dash-delta.vercel.app', 'https://dash.bundly.tech'];
     callbackUrl = 'https://bundly.tech/api';
-} else {
+    DB_URL = 'mongodb://mongo:27017/dash'
+  } else {
     // React App URL
     hosts = ['http://localhost:3000'];
     callbackUrl = 'http://localhost:5000';
+    DB_URL = 'mongodb://localhost:27017/dash'
 }
