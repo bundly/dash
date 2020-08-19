e918af667e0c55c4b82714e06249e007e2b495cc
Nischey <43901336+NB134@users.noreply.github.com>
2020-08-19 18:35:57 +0530

    Reformatted code and removed redundant files

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { default as axios } from "axios";
-import AboutUsImage from "../../../assets/Images/abstract-595.png";
+import AboutImage from "../../../assets/Images/abstract-595.png";
 import ContributeImage from "../../../assets/Images/bundlybox.png";
 import { host } from "../../../App";
 import styled from "../../../scss/login.module.scss";
@@ -17,177 +17,181 @@ const Login = ({ authenticate }) => {
   }, [authenticate]);
 
   return (
-    <div className="MainContainer">
-    <div className={styled.containerfluid}>
-      <div className={styled["landing-page-container"]}>
-        <div className={styled.content__wrapper}>
-          <header className={styled.header}>
-            <h1 className="d-flex justify-content-center display-3">BUNDLY</h1>
-          </header>
-          <p className={styled.coords}>S 75° 6' 0.027" / E 123° 19' 59.998"</p>
-          <div className={styled["ellipses-container"]}>
-            <div className={styled.greeting}></div>
-            <div
-              className={`${styled.ellipses} ${styled["ellipses__outer--thin"]}`}
-            ></div>
-            <div
-              className={`${styled.ellipses} ${styled["ellipses__outer--thick"]}`}
-            />
-          </div>
-          <div className={styled.scroller}>
-            <p className={styled["page-title"]}>MLH</p>
-            <div className={styled.timeline}>
-              <span className={styled.timeline__unit} />
-              <span
-                className={`${styled.timeline__unit} ${styled["timeline__unit--active"]}`}
+    <div className={styled["main-container"]}>
+      <div className={styled.containerfluid}>
+        <div className={styled["landing-page-container"]}>
+          <div className={styled.content__wrapper}>
+            <header className={styled.header}>
+              <h1 className="d-flex justify-content-center display-3">
+                BUNDLY
+              </h1>
+            </header>
+            <p className={styled.coords}>
+              S 75° 6' 0.027" / E 123° 19' 59.998"
+            </p>
+            <div className={styled["ellipses-container"]}>
+              <div className={styled.greeting}></div>
+              <div
+                className={`${styled.ellipses} ${styled["ellipses__outer--thin"]}`}
+              ></div>
+              <div
+                className={`${styled.ellipses} ${styled["ellipses__outer--thick"]}`}
               />
-              <span className={styled.timeline__unit} />
             </div>
-          </div>
-          <div className="text-center">
-            <div className={styled["login-github"]}>
-              <a
-                className={`${styled.button} ${styled.btnSlideEffect} ${styled["button--social-login"]} ${styled["button--github"]}`}
-                href={loginUrl}
-              >
-                Login With Github
-              </a>
+            <div className={styled.scroller}>
+              <p className={styled["page-title"]}>MLH</p>
+              <div className={styled.timeline}>
+                <span className={styled.timeline__unit} />
+                <span
+                  className={`${styled.timeline__unit} ${styled["timeline__unit--active"]}`}
+                />
+                <span className={styled.timeline__unit} />
+              </div>
+            </div>
+            <div className="text-center">
+              <div className={styled["login-github"]}>
+                <a
+                  className={`${styled.button} ${styled.btnSlideEffect} ${styled["button--social-login"]} ${styled["button--github"]}`}
+                  href={loginUrl}
+                >
+                  Login With Github
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-{/* Features Section */}
-
-<div className={styled.FeaturessContainer}>
-
-    <div className={`${styled.Features} ${styled.slide}`}>
-          <div className={styled.TextContainer}>
-                <div className={styled.IntroText}>
-                    <h1>Generate Standup Notes in markdown automatically</h1>
-                    <p>
-                    Bundly Looks through the Pull Requests Reviewed, Issues inreracted with, Pull requests opened, commits made and even the previous day's standup and generates standup notes.
-                    </p>
-                    <p>
-                    Also supports people in multiple pods and shows sugessions from previous day's standup notes from all pods !
-                    </p>
-                </div>
-          </div>
-          <div className={styled.GifCard}>
-              <img src="https://user-images.githubusercontent.com/11258286/87809620-50c66a00-c879-11ea-8f1b-b7885c828333.gif" alt="gif-Feature">
-              </img>
-          </div>
-    </div>
-
-    <div className={`${styled.Features} ${styled.slide}`} >
-          <div className={styled.TextContainer}>
-                <div className={styled.IntroText}>
-                    <h1>Access internal discussions instantly</h1>
-                    <p>
-                    Quickly search through MLH Fellowship Org discussions with the search tool. 
-                    </p>
-                </div>
-          </div>
-          <div className={styled.GifCard}>
-              <img src="https://user-images.githubusercontent.com/28642011/87812445-d6e4af80-c87d-11ea-923e-5efeebf950dc.gif" alt="gif-Feature"></img>
-          </div>
-    </div>
-
-    <div className={`${styled.Features} ${styled.slide}`} >
-          <div className={styled.TextContainer}>
-                <div className={styled.IntroText}>
-                    <h1>Never miss out on the important stuff</h1>
-                    <p>Receive personalized notifications for your repositories including:</p>
-                    <ul>
-                      <li>Issues and their comments</li>
-                      <li>Pull Requests and their comments</li>
-                      <li>Comments on any commits</li>
-                    </ul>
-
-                    Notifications are also sent for conversations in unwatched repositories when the user is involved including: 
-                    <ul>
-                      <li>@mentions</li>
-                      <li>Issue assignments</li>
-                      <li>Commits the user authors or commits</li>
-                      <li>Any discussion in which the user actively participates</li>
-                    </ul>
-                </div>
-          </div>
-          <div className={styled.GifCard}>
-              <img src="https://user-images.githubusercontent.com/11258286/87809923-cfbba280-c879-11ea-8364-a0dee9df61e2.gif" alt="gif-Feature"></img>
-          </div>
-    </div>
-
-    <div className={`${styled.Features} ${styled.slide}`} >
-            <div className={styled.TextContainer}>
-                
-                <div className={styled.IntroText}>
-                    <h1>Track tasks easily</h1>
-                    <p>
-                    Add notifications from github to your ToDo list directly 
-                    </p>
-                </div>
+      <div className={styled["features-container"]}>
+        <div className={`${styled.features} ${styled.slide}`}>
+          <div className={styled["text-container"]}>
+            <div className={styled["intro-text"]}>
+              <h1>Generate Standup Notes in markdown automatically</h1>
+              <p>
+                Bundly Looks through the Pull Requests Reviewed, Issues
+                inreracted with, Pull requests opened, commits made and even the
+                previous day's standup and generates standup notes.
+              </p>
+              <p>
+                Also supports people in multiple pods and shows sugessions from
+                previous day's standup notes from all pods !
+              </p>
             </div>
-            <div className={styled.GifCard}>
-              <img src="https://user-images.githubusercontent.com/11258286/87809923-cfbba280-c879-11ea-8364-a0dee9df61e2.gif" alt="gif-Feature"></img>
-            </div>
-    </div>
-  </div>
-
-
-
-{/* AboutUS */}
-  <div className={`${styled.AboutUs} ${styled.slide}`} >
-            <div className={styled.TextContainer}>
-                <div className={styled.IntroText}>
-                    <h1>What is Bundly?</h1>
-                    <ul>
-                      <li>
-                      Let Bundly do its work, so you can focus on yours.
-                      </li>
-                      <li>
-                      Bundly is a single heaven to find all your information regarding the MLH Fellowship at once place!
-                      </li>
-                      <li>
-                      It let's you take your utility tools, a step further. Designed specifically to enhance your Fellowship experience.
-                      </li>
-                    </ul>
-                </div>
-            </div>
-            <div className={styled.GifCard}>
-              <img src={AboutUsImage} alt="AboutUS"></img>
-            </div>
-  </div>
-
-
-
-{/* Contribute Section */}
-        <div className={`${styled.Contri} ${styled.slide}`}>
-          <div className={styled.ImgContainer}>
-            <img src={ContributeImage} alt="ContributeImg">
-
-            </img>
-            <h2>Let's set up your developer dashboard</h2>
           </div>
-          <div className={styled.BtnContainer}>
-            <a href={loginUrl} className={`${styled.ContriButton}`}>
-              Login to Bundly
-            </a>
-            <a href="https://github.com/bundly/dash/blob/master/CONTRIBUTING.md" className={`${styled.ContriButton}`}>
-              Contribute to development
-            </a>
+          <div className={styled["gif-card"]}>
+            <img
+              src="https://user-images.githubusercontent.com/11258286/87809620-50c66a00-c879-11ea-8f1b-b7885c828333.gif"
+              alt="gif-Feature"
+            ></img>
           </div>
         </div>
 
-
-{/* Copyright */}
-          <div className={styled.Copyright}>
-            Copyright &copy; 2020 Bundly
+        <div className={`${styled.features} ${styled.slide}`}>
+          <div className={styled["text-container"]}>
+            <div className={styled["intro-text"]}>
+              <h1>Access internal discussions instantly</h1>
+              <p>
+                Quickly search through MLH Fellowship Org discussions with the
+                search tool.
+              </p>
+            </div>
           </div>
+          <div className={styled["gif-card"]}>
+            <img
+              src="https://user-images.githubusercontent.com/28642011/87812445-d6e4af80-c87d-11ea-923e-5efeebf950dc.gif"
+              alt="gif-Feature"
+            ></img>
+          </div>
+        </div>
 
+        <div className={`${styled.features} ${styled.slide}`}>
+          <div className={styled["text-container"]}>
+            <div className={styled["intro-text"]}>
+              <h1>Never miss out on the important stuff</h1>
+              <p>
+                Receive personalized notifications for your repositories
+                including:
+              </p>
+              <ul>
+                <li>Issues and their comments</li>
+                <li>Pull Requests and their comments</li>
+                <li>Comments on any commits</li>
+              </ul>
+              Notifications are also sent for conversations in unwatched
+              repositories when the user is involved including:
+              <ul>
+                <li>@mentions</li>
+                <li>Issue assignments</li>
+                <li>Commits the user authors or commits</li>
+                <li>Any discussion in which the user actively participates</li>
+              </ul>
+            </div>
+          </div>
+          <div className={styled["gif-card"]}>
+            <img
+              src="https://user-images.githubusercontent.com/11258286/87809923-cfbba280-c879-11ea-8364-a0dee9df61e2.gif"
+              alt="gif-Feature"
+            ></img>
+          </div>
+        </div>
 
+        <div className={`${styled.features} ${styled.slide}`}>
+          <div className={styled["text-container"]}>
+            <div className={styled["intro-text"]}>
+              <h1>Track tasks easily</h1>
+              <p>Add notifications from github to your ToDo list directly</p>
+            </div>
+          </div>
+          <div className={styled["gif-card"]}>
+            <img
+              src="https://user-images.githubusercontent.com/11258286/87809923-cfbba280-c879-11ea-8364-a0dee9df61e2.gif"
+              alt="gif-Feature"
+            ></img>
+          </div>
+        </div>
+      </div>
 
+      <div className={`${styled.about} ${styled.slide}`}>
+        <div className={styled["text-container"]}>
+          <div className={styled["intro-text"]}>
+            <h1>What is Bundly?</h1>
+            <ul>
+              <li>Let Bundly do its work, so you can focus on yours.</li>
+              <li>
+                Bundly is a single heaven to find all your information regarding
+                the MLH Fellowship at once place!
+              </li>
+              <li>
+                It let's you take your utility tools, a step further. Designed
+                specifically to enhance your Fellowship experience.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styled["gif-card"]}>
+          <img src={AboutImage} alt="About"></img>
+        </div>
+      </div>
+
+      <div className={`${styled["contribute"]} ${styled.slide}`}>
+        <div className={styled["img-container"]}>
+          <img src={ContributeImage} alt="ContributeImg"></img>
+          <h2>Let's set up your developer dashboard</h2>
+        </div>
+        <div className={styled["btn-container"]}>
+          <a href={loginUrl} className={`${styled["contribute-button"]}`}>
+            Login to Bundly
+          </a>
+          <a
+            href="https://github.com/bundly/dash/blob/master/CONTRIBUTING.md"
+            className={`${styled["contribute-button"]}`}
+          >
+            Contribute to development
+          </a>
+        </div>
+      </div>
+
+      <div className={styled.copyright}>Copyright &copy; 2020 Bundly</div>
     </div>
   );
 };
